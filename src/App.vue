142e34958ed9a99279933c46a72db277d65d31a8
P4UL-M <paul.mairesse@free.fr>
2024-10-02 11:13:03 +0200

    Ex 6 : create BaseLayout that use slot api

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,23 @@
 <template>
   <div>
-    <base-header />
-    <home-page />
-    <base-footer />
+    <base-layout>
+      <home-page />
+    </base-layout>
   </div>
 </template>
 
 <script>
 import HomePage from './components/HomePage.vue';
-import BaseFooter from './components/BaseFooter.vue';
-import BaseHeader from './components/BaseHeader.vue';
+import BaseLayout from './components/BaseLayout.vue';
 
 
 export default {
   name: 'App',
   components: {
     HomePage,
-    BaseFooter,
-    BaseHeader
+    BaseLayout
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  /* margin-top: 60px; */
-  margin: 0 0;
-}
-
-body {
-  margin: 0;
-  padding: 0;
-}
-</style>
+<style></style>
---
+++ src/components/BaseLayout.vue
@@ -0,0 +1,39 @@
+<template>
+    <base-header />
+    <slot></slot>
+    <base-footer />
+</template>
+
+<script>
+
+import BaseFooter from './BaseFooter.vue';
+import BaseHeader from './BaseHeader.vue';
+
+export default {
+    name: 'BaseLayout',
+    components: {
+        BaseFooter,
+        BaseHeader
+    }
+}
+</script>
+
+
+
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    /* margin-top: 60px; */
+    margin: 0 0;
+}
+
+body {
+    margin: 0;
+    padding: 0;
+}
+</style>
